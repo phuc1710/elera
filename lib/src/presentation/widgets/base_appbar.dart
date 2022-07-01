a37d9da0301daf002a95c4eabdc5fa11d294e3a6
Thinh Hua <hungthinh.itj@gmail.com>
2022-07-01 15:05:54 +0700

    feat: add appbar widget

--- lib/src/presentation/widgets/base_appbar.dart
+++ lib/src/presentation/widgets/base_appbar.dart
@@ -11,25 +11,56 @@ AppBar buildAppBar(AppBarParams params) {
       style: Theme.of(params.context).textTheme.titleLarge,
     ),
     centerTitle: params.centerTitle,
-    leading: params.hasBack
-        ? (params.backButton ??
-            IconButton(
-              onPressed: params.onExit ??
-                  () {
-                    Navigator.pop(params.context);
-                  },
-              iconSize: 30,
-              icon: const Icon(Icons.arrow_back, color: Colors.black),
-            ))
-        : null,
-    actions: [
-      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: const Icon(
-          Icons.more_horiz_rounded,
-          color: Colors.black,
-        ),
-      )
-    ],
+    leading: params.leading ??
+        (params.hasBack
+            ? (params.backButton ??
+                IconButton(
+                  onPressed: params.onExit ??
+                      () {
+                        Navigator.pop(params.context);
+                      },
+                  iconSize: 30,
+                  icon: const Icon(Icons.arrow_back, color: Colors.black),
+                ))
+            : null),
+    actions: params.actions,
   );
+}
+
+class TopAppBar extends StatelessWidget {
+  const TopAppBar(
+    this.params, {
+    Key? key,
+  }) : super(key: key);
+
+  final AppBarParams params;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 70,
+      width: double.maxFinite,
+      child: Row(
+        children: [
+          if (params.leading != null) params.leading!,
+          const SizedBox(width: 10),
+          Expanded(
+            child: Text(
+              params.title ?? '',
+              style: Theme.of(params.context).textTheme.titleMedium,
+            ),
+          ),
+          if (params.actions.isNotEmpty)
+            ...params.actions
+                .map(
+                  (e) => Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: e,
+                  ),
+                )
+                .toList(),
+        ],
+      ),
+    );
+  }
 }
