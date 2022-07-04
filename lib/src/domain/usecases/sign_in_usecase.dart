e9094f422142381ea5da190d00a02f819e558f7f
phucndh <contact.phuc1710@gmail.com>
2022-07-04 10:43:43 +0700

    refactor: rename sign in repo and mockup file

--- lib/src/domain/usecases/sign_in_usecase.dart
+++ lib/src/domain/usecases/sign_in_usecase.dart
@@ -2,16 +2,16 @@ import '../../core/params/sign_in_request_params.dart';
 import '../../core/resources/data_state.dart';
 import '../../core/usecases/usecases.dart';
 import '../../data/models/sign_in/sign_in_response_model.dart';
-import '../repositories/sign_in_repositories.dart';
+import '../repositories/user_repository.dart';
 
 class SignInUseCase
     implements UseCase<DataState<SignInResponseModel>, SignInRequestParams> {
-  SignInUseCase(this._signInRepository);
+  SignInUseCase(this._userRepository);
 
-  final SignInRepository _signInRepository;
+  final UserRepository _userRepository;
 
   @override
   Future<DataState<SignInResponseModel>> call({SignInRequestParams? params}) {
-    return _signInRepository.postSignInRequest(params);
+    return _userRepository.postSignInRequest(params);
   }
 }
