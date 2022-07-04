8790132d2acc7a962d1425b20c243942fdafa8cb
phucndh <contact.phuc1710@gmail.com>
2022-07-04 10:38:02 +0700

    fix: add google font license

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/config/theme/app_theme.dart';
 import 'src/injector.dart';
 import 'src/presentation/onboarding/intro/views/intro_view.dart';
 
 Future<void> main() async {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
+
   runApp(const App());
 }
 
