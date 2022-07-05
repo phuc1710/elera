787e91d9647ca60dde748defb6703cbcb1847591
phucndh <contact.phuc1710@gmail.com>
2022-07-05 14:51:47 +0700

    feat: fill your profile image picker

--- lib/src/presentation/account_setup/fill_your_profile/widgets/scaffold_body.dart
+++ lib/src/presentation/account_setup/fill_your_profile/widgets/scaffold_body.dart
@@ -4,6 +4,7 @@ import '../widgets/gender_dropdown.dart';
 import '../widgets/information_input.dart';
 import '../widgets/main_action_button.dart';
 import '../widgets/phone_number_input.dart';
+import 'avatar.dart';
 
 class ScaffoldBody extends StatefulWidget {
   const ScaffoldBody({Key? key}) : super(key: key);
@@ -20,24 +21,23 @@ class _ScaffoldBodyState extends State<ScaffoldBody> {
         horizontal: MediaQuery.of(context).size.width * 0.05,
       ),
       child: ListView(
-        children: [
-          CircleAvatar(
-            radius: MediaQuery.of(context).size.width * 0.17,
-            backgroundColor: Colors.red,
-          ),
-          const InformationInput(hintText: 'Full Name'),
-          const InformationInput(hintText: 'Nickname'),
-          const InformationInput(
+        children: const [
+          Avatar(),
+          InformationInput(hintText: 'Full Name'),
+          InformationInput(hintText: 'Nickname'),
+          // TODO(phucndh): implement date_picker from thinhhh
+          InformationInput(
             hintText: 'Date of Birth',
             suffixIcon: Icon(Icons.date_range_rounded),
           ),
-          const InformationInput(
+          InformationInput(
             hintText: 'Email',
             suffixIcon: Icon(Icons.email_rounded),
           ),
-          const PhoneNumberInput(),
-          const GenderDropdown(),
-          const MainActionButton()
+          // TODO(phucndh): implement phone input from thinhhh
+          PhoneNumberInput(),
+          GenderDropdown(),
+          MainActionButton()
         ],
       ),
     );
