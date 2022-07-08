f8093ef373367ae00e09e4f6d03b31d7ac7b7ab8
phucndh <contact.phuc1710@gmail.com>
2022-07-08 09:34:30 +0700

    feat: enter pin resend code button ui

--- lib/src/presentation/forgot_and_reset_password/enter_pin/widgets/resend_code_row.dart
+++ lib/src/presentation/forgot_and_reset_password/enter_pin/widgets/resend_code_row.dart
@@ -1,38 +1,53 @@
 import 'package:flutter/material.dart';
 
-class ResendCodeRow extends StatelessWidget {
+class ResendCodeRow extends StatefulWidget {
   const ResendCodeRow({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<ResendCodeRow> createState() => _ResendCodeRowState();
+}
+
+class _ResendCodeRowState extends State<ResendCodeRow> {
+  bool isTimeOut = false;
+  @override
   Widget build(BuildContext context) {
+    final TextStyle? displayMedium = Theme.of(context).textTheme.displayMedium;
+
     return Expanded(
       flex: 2,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Resend code in ',
-            style: Theme.of(context).textTheme.displayMedium,
-          ),
-          TweenAnimationBuilder(
-            tween: Tween(begin: const Duration(minutes: 1), end: Duration.zero),
-            duration: const Duration(minutes: 1),
+          Text('Resend code in ', style: displayMedium),
+          buildResendTimerAndButton(),
+          Text(' s', style: displayMedium),
+        ],
+      ),
+    );
+  }
+
+  Widget buildResendTimerAndButton() {
+    const Duration duration = Duration(minutes: 1);
+
+    return !isTimeOut
+        ? TweenAnimationBuilder(
+            tween: Tween(begin: duration, end: Duration.zero),
+            duration: duration,
             builder: (BuildContext context, Duration duration, Widget? child) {
               return Text(
                 '${duration.inSeconds}',
                 style: Theme.of(context).textTheme.labelMedium,
               );
             },
-          ),
-          Text(
-            ' s',
-            style: Theme.of(context).textTheme.displayMedium,
-          ),
-        ],
-      ),
-    );
+            onEnd: () => setState(() => isTimeOut = true),
+          )
+        : InkWell(
+            onTap: () => setState(() => isTimeOut = false),
+            child:
+                Text('Resend', style: Theme.of(context).textTheme.labelMedium),
+          );
   }
 }
