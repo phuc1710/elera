a7bddcc0250c7f012858e4ee863360762d2780c3
phucndh <contact.phuc1710@gmail.com>
2022-08-31 09:46:34 +0700

    refactor: update code gen packages

--- app/lib/src/data/datasources/remote/bookmark_api_service.g.dart
+++ app/lib/src/data/datasources/remote/bookmark_api_service.g.dart
@@ -6,7 +6,7 @@ part of 'bookmark_api_service.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
 
 class _BookmarkApiService implements BookmarkApiService {
   _BookmarkApiService(this._dio, {this.baseUrl}) {
